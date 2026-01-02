0c9006dfbcfc88474a4303331ec6cd96938cddf7
cedric <149272209+cjeannoel2005@users.noreply.github.com>
2026-01-01 23:10:34 -0500

    Update decision-table-code-demo.java
    added the code for the code of the decision table

--- Requirements/decision-table-code-demo.java
+++ Requirements/decision-table-code-demo.java
@@ -1,1 +1,38 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
 
+        boolean accountCreation = true;
+        boolean accountlogin = true;
+        
+        
+         Scanner scanner = new Scanner(System.in);
+          System.out.println("do you have an account ? ");
+         String customerInput1 = scanner.nextLine();
+        System.out.println("what type of account do you have or wish to create ?");
+         String customerInput2 = scanner.nextLine();
+         
+          if ( customerInput1.equals("yes") || customerInput2.equals("customer") ) {
+             accountCreation = false;
+             accountlogin = true;
+               System.out.println("please enter your credentials to login");
+               
+               // after login you will be taken to the main page then will be able to checkout and so on
+               
+          }
+          else if ( customerInput1.equals("no") || customerInput2.equals("customer")) {
+              System.out.println("Please follow the appropriate steps to create an account");
+              accountCreation = true;
+              accountlogin = false;
+          }
+          else if ( customerInput1.equals("yes") || customerInput2.equals("admin")) {
+              accountCreation = false;
+              accountlogin = true;
+               System.out.println("please click on either of the following pages to view them");
+               System.out.println(" report page and export to csv");
+          }
+          
+        }
+
+
+}
