30f791fc7fd50634c67054316379de8919ce990f
cedric <149272209+cjeannoel2005@users.noreply.github.com>
2026-01-11 00:21:07 -0500

    Add spring boot project structure and source code

---
+++ src/main/java/com/sspstores/sspstore/SSPStoreApplication.java
@@ -0,0 +1,12 @@
+package com.sspstores.sspstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSPStoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SSPStoreApplication.class, args);
+        System.out.println("cedric was here "),
+    }
+}
