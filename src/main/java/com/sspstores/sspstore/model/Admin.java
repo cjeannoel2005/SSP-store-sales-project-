b57b47a33208f87e23f7699620521fb3e5974b3b
cedric <cojeannoel@gmail.com>
2026-01-21 23:31:30 -0500

    Adding the correct structure to the class for the database

--- src/main/java/com/sspstores/sspstore/model/Admin.java
+++ src/main/java/com/sspstores/sspstore/model/Admin.java
@@ -1,11 +1,18 @@
+// packages tell the proram the location and type of file
 package com.sspstores.sspstore.model;
 
+// imports for the database
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+// naming the db table admins
 @Entity
 @Table((name="Admins")
  public class Admin{
 
- @Id
-        @GeneratedValue(stategy = GenerationType.IDENTIY)
+ @Id @GeneratedValue(stategy = GenerationType.IDENTIY)
 
     @Column(name="adminUsername",nullable = false, unique = true)
         private String adminUsername;
@@ -14,4 +21,5 @@ package com.sspstores.sspstore.model;
         private String AdminPassword;
 
    // after this connect the field up there to the other entities
+
  }
